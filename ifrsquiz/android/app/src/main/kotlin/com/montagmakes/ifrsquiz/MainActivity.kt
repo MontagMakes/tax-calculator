c3f78b23d49b531ea889e5faeff59d393e7101b7
Arsalan Khan Nizamani <103876121+MontagMakes@users.noreply.github.com>
2024-06-13 10:11:35 +0500

    Delete ifrsquiz directory

--- ifrsquiz/android/app/src/main/kotlin/com/montagmakes/ifrsquiz/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.montagmakes.ifrsquiz
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- ifrsquiz/lib/globals/globals.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Globals {
-  static const Color primaryColor = Color.fromARGB(255, 164, 31, 54);
-}
